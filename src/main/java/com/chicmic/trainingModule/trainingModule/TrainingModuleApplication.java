a3c7ac010a1a19df4d6b877aa08e6cdb58557b87
itsAbhi <abhijeet.rishi@chcimic.co.in>
2023-11-30 14:52:04 +0530

    configure mongodb properties in application.properties

--- src/main/java/com/chicmic/trainingModule/trainingModule/TrainingModuleApplication.java
+++ src/main/java/com/chicmic/trainingModule/trainingModule/TrainingModuleApplication.java
@@ -2,8 +2,9 @@ package com.chicmic.trainingModule.trainingModule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TrainingModuleApplication {
 
 	public static void main(String[] args) {
