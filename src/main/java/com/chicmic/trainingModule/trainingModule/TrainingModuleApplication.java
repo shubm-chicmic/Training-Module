b77c2c8e053bb241c183a3bac3aabeb14011317b
shubm-chicmic <shubham.mishra@chicmic.co.in>
2023-11-30 13:55:25 +0530

    git Initialisation

---
+++ src/main/java/com/chicmic/trainingModule/trainingModule/TrainingModuleApplication.java
@@ -0,0 +1,13 @@
+package com.chicmic.trainingModule.trainingModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingModuleApplication.class, args);
+	}
+
+}
