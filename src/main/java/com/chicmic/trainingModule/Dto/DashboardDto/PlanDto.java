1827be3886044e5b5388c9e5face3b213b482a0e
itsAbhi <abhijeet.rishi@chcimic.co.in>
2023-12-22 14:39:08 +0530

    create : custom comparable for sorting PlanDto

--- src/main/java/com/chicmic/trainingModule/Dto/DashboardDto/PlanDto.java
+++ src/main/java/com/chicmic/trainingModule/Dto/DashboardDto/PlanDto.java
@@ -5,10 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 @Builder @Getter @Setter
-public class PlanDto {
+public class PlanDto implements Comparable<PlanDto>{
     private String name;
     private String date;
     private String phase;
+    private Integer type;
     @JsonProperty("isComplete")
     private boolean isComplete;
+
+    @Override
+    public int compareTo(PlanDto o) {
+        int a = 1 - this.getType();
+        int b = 1 - o.getType();
+        return Integer.compare(a,b);
+    }
 }
