ec4cd53bf203b1909dbd3912f48752caa57129dd
itsAbhi <abhijeet.rishi@chcimic.co.in>
2024-01-05 15:44:27 +0530

    update : traineeDashboard

--- src/main/java/com/chicmic/trainingModule/Service/DashboardService/DashboardService_V2.java
+++ src/main/java/com/chicmic/trainingModule/Service/DashboardService/DashboardService_V2.java
@@ -1,13 +1,11 @@
 package com.chicmic.trainingModule.Service.DashboardService;
 
+
 import com.chicmic.trainingModule.Dto.DashboardDto.CourseDto;
 import com.chicmic.trainingModule.Dto.DashboardDto.DashboardResponse;
 import com.chicmic.trainingModule.Dto.DashboardDto.PlanDto;
 import com.chicmic.trainingModule.Dto.UserDto;
-import com.chicmic.trainingModule.Entity.AssignedPlan;
-import com.chicmic.trainingModule.Entity.Phase;
-import com.chicmic.trainingModule.Entity.Plan;
-import com.chicmic.trainingModule.Entity.PlanTask;
+import com.chicmic.trainingModule.Entity.*;
 import com.chicmic.trainingModule.Service.FeedBackService.FeedbackService_V2;
 import com.chicmic.trainingModule.TrainingModuleApplication;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -18,6 +16,8 @@ import org.springframework.stereotype.Service;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
+
+import static com.chicmic.trainingModule.Entity.Constants.EntityType.COURSE;
 
 @Service
 public class DashboardService_V2 {
@@ -48,72 +48,25 @@ public class DashboardService_V2 {
         List<PlanDto> planDtoList = new ArrayList<>();
         plans.forEach((p)->{
             p.getPhases().forEach(ps -> {
-                if (ps.getEntityType() == 1) courseDtoList.add(new CourseDto(ps.getName(),50));
+                ps.getTasks().forEach(pt -> {
+                    if (pt != null && pt instanceof PlanTask && pt.getPlanType() == COURSE)
+                        courseDtoList.add(new CourseDto(pt.getPlan(),(pt.getTotalTasks()%10) * 10));
+                });
                 planDtoList.add(PlanDto.builder().name(p.getPlanName()).phase(ps.getName())
                         .isComplete(false).date(formatter.format(new Date())).build());
                     //planId.set(p.get_id());
             });
         });
+        List<String> courseIds = new ArrayList<>();
+        courseDtoList.forEach(c -> courseIds.add(c.getName()));
+        //get courses name
+        Criteria criteria1 = Criteria.where("_id").in(courseIds);
+        List<Course> courseList = mongoTemplate.find(new Query(criteria1), Course.class);
+        Map<String,String> positions = new HashMap<>();
+        for (int i=0;i<courseList.size();i++) positions.put(courseList.get(i).get_id(),courseList.get(i).getName());
+        courseDtoList.forEach(c -> c.setName(positions.get(c.getName())));
         dashboardResponse.setCourses(courseDtoList);
         dashboardResponse.setPlan(planDtoList);
-//        Map<String,List<AssignTaskPlanTrack>> assignCourseMap  = new HashMap<>();
-//        Map<String,List<AssignTaskPlanTrack>> assignTestMap = new HashMap<>();
-//        List<PlanDto> planDtoList = new ArrayList<>();
-//        Date date  = new Date();
-//        if(assignTask != null) {
-//            List<Plan> plans = assignTask.getPlans();
-//            List<CourseDto> courseDtos = new ArrayList<>();
-//            List<String> courseIds = new ArrayList<>();
-//            List<String> testIds = new ArrayList<>();
-//            for (Plan plan : plans) {
-//                for (Phase phase : plan.getPhases()) {
-//                    for (PlanTask planTask : phase.getTasks()) {
-//                        String _id = ((AssignTaskPlanTrack) planTask.getPlan()).get_id();
-//                        if(planTask.getPlanType() != 3)
-//                            planDtoList.add(PlanDto.builder().name(phase.getPhaseName()).date(formatter.format(date)).isComplete(planTask.getIsCompleted())
-//                                .phase(_id).type(planTask.getPlanType()).build());
-//                        //AssignTaskPlanTrack assignTaskPlanTrack = ((AssignTaskPlanTrack) task.getPlan());
-//                        List<AssignTaskPlanTrack> milestones = (List<AssignTaskPlanTrack>) planTask.getPhases();
-//                        if(planTask.getPlanType() == 2){
-//                            assignTestMap.put(_id,milestones);
-//                            testIds.add(_id);
-//                        }
-//                        if(planTask.getPlanType() != 1) continue;
-//                        int count = 0;
-//                        for (AssignTaskPlanTrack milestone : milestones) {
-//                            if (milestone.getIsCompleted() == true)
-//                                ++count;
-//                        }
-//                        courseIds.add(_id);
-//                        courseDtos.add(new CourseDto(_id, count*100/Integer.max(1,milestones.size())));
-//                        assignCourseMap.put(_id,milestones);
-//                        // courseId.put(_id,count/ milestones.size() * 100);
-//                    }
-//                }
-//            }
-//            Map<String, Document> courseDetails = feedbackService.getCourseNameAndPhaseName(courseIds);
-//            Map<String,Document> testDetails = feedbackService.getTestNameAndMilestoneName(testIds);
-//            for (CourseDto courseDto : courseDtos) {
-//                String _id = courseDto.getName();
-//                String tp = (String) courseDetails.get(_id).get("name");
-//                courseDto.setName(tp);
-//            }
-//            for(PlanDto planDto : planDtoList){
-//                String _id = planDto.getPhase();
-//                int type = Integer.min(3.planDt)
-//                if(planDto.getType()==1||planDto.getType()==4){
-//                    String tp = (String) courseDetails.get(_id).get("name");
-//                    planDto.setPhase(tp);
-//                }else if(planDto.getType()==2){
-//                    String tp = (String) testDetails.get(_id).get("testName");
-//                    planDto.setPhase(tp);
-//                }
-//            }
-//            Collections.sort(planDtoList);
-//            dashboardResponse.setCourses(courseDtos);
-//            dashboardResponse.setPlan(planDtoList);
-            //---set plan details!!!
-//        }
         return dashboardResponse;
     }
 }
