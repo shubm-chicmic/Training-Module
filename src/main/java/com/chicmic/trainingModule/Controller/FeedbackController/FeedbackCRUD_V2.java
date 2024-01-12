b60f1370bc422160295597fb6591726e0f0f551c
itsAbhi <abhijeet.rishi@chcimic.co.in>
2024-01-12 14:36:22 +0530

    update : name of feedbackCrud Api

--- src/main/java/com/chicmic/trainingModule/Controller/FeedbackController/FeedbackCRUD_V2.java
+++ src/main/java/com/chicmic/trainingModule/Controller/FeedbackController/FeedbackCRUD_V2.java
@@ -22,7 +22,7 @@ import static com.chicmic.trainingModule.Dto.FeedbackResponseDto_V2.FeedbackResp
 import static com.chicmic.trainingModule.Util.FeedbackUtil.checkRole;
 
 @RestController
-@RequestMapping("/v2/training/feedback")
+@RequestMapping("/v1/training/feedback")
 public class FeedbackCRUD_V2 {
     private FeedbackService_V2 feedbackService;
 
